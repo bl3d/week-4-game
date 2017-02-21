61b163d7d20b3e2a5fc1a54459c074ef67e90681
bl3d <mr.brentlewis@gmail.com>
2017-02-20 20:49:46 -0500

    initial assets commit

---
+++ assets/javascript/game.js
@@ -0,0 +1,41 @@
+(function() {
+  'use strict'; 
+
+
+
+	// 
+	var sw = {
+
+	   init: function(){
+
+	   		
+
+	   }
+
+	};
+
+
+	// kick off game 
+	sw.init(); 
+
+	
+
+
+
+
+
+
+
+
+
+
+	//user events
+	//**************************************************//
+
+
+
+
+
+
+
+}()); 
